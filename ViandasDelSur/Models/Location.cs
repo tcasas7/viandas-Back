ce2c302817281d8b5c583d194678f00d997d3674
Tomas Casas <tomas.casas2001@gmail.com>
2025-01-21 14:40:54 -0300

    Direcciones andando correctamente

--- ViandasDelSur/Models/Location.cs
+++ ViandasDelSur/Models/Location.cs
@@ -7,8 +7,8 @@
         public bool isDefault { get; set; }
         public int userId { get; set; }
         public User User { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
 
     }
 }
--- ViandasDelSur/Repositories/Interfaces/ILocationRepository.cs
+++ ViandasDelSur/Repositories/Interfaces/ILocationRepository.cs
@@ -1,12 +1,16 @@
-﻿using ViandasDelSur.Models;
+﻿using System.Collections.Generic;
+using ViandasDelSur.Models;
 
 namespace ViandasDelSur.Repositories.Interfaces
 {
     public interface ILocationRepository
     {
-        void Save(Location location);
+        
+        bool Save(Location location);
         Location FindById(long id);
+        Location FindByUserIdAndDir(long userId, string dir);
         Location GetDefault(string email);
+        IEnumerable<Location> GetLocationsByUserId(long userId);
         void Remove(Location location);
     }
 }
